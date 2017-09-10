606d9e5d49ec4993779de44597672e6796e1dfd1
vegaralstad <vegar.alstad@gmail.com>
2017-09-10 23:01:00 +0200

    endring 2 are you good bro

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,7 +1,16 @@
 print ("Are you good bro?")
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 var = input("Yes/no: ")
 print ("Thank you for your respons"), var,
 
-   
+  
