1752479126f79dd8e0fccb248c109a6ac34bad9c
vegaralstad <vegar.alstad@gmail.com>
2017-09-11 21:53:42 +0200

    test 5

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,10 +1,16 @@
+
 print ("Are you good bro?")
 
 
 var = input("Yes/no: ")
-if var == "yes": import webbrowser 
+if var == "yes": import webbrowser
 new=2;
 url="https://www.youtube.com/watch?v=6xsDdIByh8A"
-
 webbrowser.open(url,new=new);
-print ("Thank you for your respons"), var,
+
+if var== "no": import webbrowser
+new=2;
+url="https://www.youtube.com/watch?v=6xsDdIByh8A"
+webbrowser.open(url,new=new);
+ 
+print ("Thank you for your respons"), var, 
